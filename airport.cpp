579985abb5b8506a873b3f52a9f0a56ba52f1138
Matteo Pasotti <matteo@xquiet.eu>
2012-09-29 16:08:46 +0200

    decode icao to build airport directory path

--- airport.cpp
+++ airport.cpp
@@ -1,5 +1,18 @@
 #include "airport.h"
 
-Airport::Airport(QString airportDir, QString icao)
+Airport::Airport(QString airportsDir, QString icao)
 {
+    // build airport directory path
+    const char *ch_icao;
+    ch_icao = icao.toStdString().c_str();
+    QString I(ch_icao[0]);
+    QString C(ch_icao[1]);
+    QString A(ch_icao[2]);
+
+    airportPath = airportsDir + "/" + I + "/" + C + "/" + A;
+}
+
+QString Airport::getAirportDirPath()
+{
+    return airportPath;
 }
--- airport.h
+++ airport.h
@@ -2,20 +2,23 @@
 #define AIRPORT_H
 
 #include <QStringList>
+//#include <QDir>
 
 #include "runway.h"
 
 class Airport
 {
 public:
-    Airport(QString airportDir, QString icao); // icao from Airports/index.txt
-
+    Airport(QString airportsDir, QString icao); // icao from Airports/index.txt
     // get
     QString getICAO();
     QString getLongitude();
     QString getLatitude();
     QStringList getRunwayList();
     Runway getRunway();
+    QString getAirportDirPath();
+private:
+    QString airportPath;
 };
 
 #endif // AIRPORT_H
