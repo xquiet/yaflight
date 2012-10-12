fce1308432ceb6705e8be45e59061c51c8807b95
Matteo <matteo@localhost.localdomain>
2012-10-12 14:28:00 +0200

    fixed issue on building airport directory from icao code

--- airport.cpp
+++ airport.cpp
@@ -3,12 +3,17 @@
 Airport::Airport(QString airportsDir, QString icao)
 {
     // build airport directory path
-    const char *ch_icao;
-    ch_icao = icao.toStdString().c_str();
-    QString I(ch_icao[0]);
-    QString C(ch_icao[1]);
-    QString A(ch_icao[2]);
-
+    QStringList prova = icao.trimmed().split("");
+    // ottengo un vettore di 6 elementi
+    // i cui estremi non mi interessano
+    prova.pop_front();
+    prova.pop_back();
+    /*const char *ch_icao;
+    ch_icao = icao.toStdString().data();*/
+    QString I(prova[0]);
+    QString C(prova[1]);
+    QString A(prova[2]);
+    //ch_icao = "";
     airportPath = airportsDir + "/" + I + "/" + C + "/" + A;
 }
 
