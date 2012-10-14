0ce324b4555571dd06772459201b17984511e92f
Matteo Pasotti <matteo@xquiet.eu>
2012-10-15 00:35:37 +0200

    replaced cyaflight with yaflight

--- aircraft.cpp
+++ aircraft.cpp
@@ -1,14 +1,14 @@
 /**  
     Copyright (C) 2012  Matteo Pasotti <matteo.pasotti@gmail.com>
 
-    This file is part of cyaflight - Cross platform YaFlight
+    This file is part of yaflight - Cross platform YaFlight
 
-    cyaflight is free software: you can redistribute it and/or modify
+    yaflight is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    cyaflight is distributed in the hope that it will be useful,
+    yaflight is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
