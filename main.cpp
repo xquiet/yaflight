b0844f3867105fc7b60760b901d673dd75c2e91f
Matteo Pasotti <matteo@xquiet.eu>
2012-10-26 01:45:32 +0200

    fixed translation support when built for windows platforms

--- main.cpp
+++ main.cpp
@@ -25,8 +25,13 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     QTranslator yaflightTranslator;
     qDebug(TRANSDIR);
+    #ifdef Q_OS_UNIX
     yaflightTranslator.load(QLocale::system().name(),
                             TRANSDIR);
+    #elif defined Q_OS_WIN32
+    yaflightTranslator.load(QLocale::system().name(),
+                            a.applicationDirPath() + "/languages");
+    #endif
     a.installTranslator(&yaflightTranslator);
     MainWindow w;
     w.show();
