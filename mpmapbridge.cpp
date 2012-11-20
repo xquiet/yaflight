c0c15bb8d32ca3842e3185d6ada578a4617da99e
Matteo Pasotti <matteo@xquiet.eu>
2012-11-20 17:04:15 +0100

    fixed class name and filename

---
+++ mpmapbridge.cpp
@@ -0,0 +1,23 @@
+#include "mpmapbridge.h"
+
+QString mpmapbridge::baseUrl = "http://mpmap02.flightgear.org/";
+
+mpmapbridge::mpmapbridge()
+{
+}
+
+QString mpmapbridge::getBaseUrl()
+{
+    return baseUrl;
+}
+
+void mpmapbridge::setLonLat(QString lon, QString lat)
+{
+    ll = new LonLat();
+    ll->set(lon, lat);
+}
+
+QString mpmapbridge::getUrl()
+{
+    return baseUrl + "?ll=" + ll->getLatLon();
+}
---
+++ mpmapbridge.h
@@ -0,0 +1,40 @@
+/**
+    Copyright (C) 2012  Matteo Pasotti <matteo.pasotti@gmail.com>
+
+    This file is part of yaflight - Cross platform YaFlight
+
+    yaflight is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    yaflight is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    */
+
+#ifndef MPMAPBRIDGE_H
+#define MPMAPBRIDGE_H
+
+#include <QString>
+
+#include "lonlat.h"
+
+class mpmapbridge
+{
+public:
+    mpmapbridge();
+    static QString getBaseUrl();
+    QString getUrl();
+    void setLonLat(QString lon, QString lat);
+
+private:
+    static QString baseUrl;
+    LonLat *ll;
+};
+
+#endif // MPMAPBRIDGE_H
