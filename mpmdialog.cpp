65e9c2afa98dcea3ae63915c937cc98f19da51d7
xquiet <matteo@xquiet.eu>
2013-10-01 00:10:42 +0200

    SILENT: dropped some garbage

--- mpmdialog.cpp
+++ mpmdialog.cpp
@@ -22,30 +22,5 @@ void MPMDialog::setup_mp()
 
 void MPMDialog::on_pbtRefreshMPMap_clicked()
 {
-//    mpmapbridge *bridge = new mpmapbridge();
-//    if(ui->ckbFollowPilot->isChecked())
-//    {
-//        bridge->setPilotToFollow(ui->lnedtPilot->text().trimmed());
-
-//        if(ui->ckbShowMPMapMenu->isChecked())
-//        {
-//            ui->webViewMMap->setUrl(bridge->getFollowPilotUrl());
-//        }
-//        else
-//        {
-//            ui->webViewMMap->setUrl(bridge->getFollowPilotUrlWithoutMenu());
-//        }
-//    }
-//    else
-//    {
-//        if(ui->ckbShowMPMapMenu->isChecked())
-//        {
-//            ui->webViewMMap->setUrl(bridge->getUrl());
-//        }
-//        else
-//        {
-//            ui->webViewMMap->setUrl(bridge->getUrlWithoutMenu());
-//        }
-//    }
     ui->webViewMMap->reload();
 }
